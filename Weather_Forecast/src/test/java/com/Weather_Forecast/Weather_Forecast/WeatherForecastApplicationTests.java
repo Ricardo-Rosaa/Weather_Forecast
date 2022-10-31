0765268cb361573b92e38584bed916b308190289
BernardoAlves05 <116270830+BernardoAlves05@users.noreply.github.com>
2022-10-31 15:22:50 +0000

    Add files via upload

---
+++ Weather_Forecast/src/test/java/com/Weather_Forecast/Weather_Forecast/WeatherForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Weather_Forecast.Weather_Forecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
